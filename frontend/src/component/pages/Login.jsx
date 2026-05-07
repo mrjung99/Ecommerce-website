2e2f2da12896c50111ee2bdcaf8434eda233bbd8
zeroCool <jungdaulat99@gmail.com>
2026-05-07 19:21:00 +0545

    change local backend url to render

--- frontend/src/component/pages/Login.jsx
+++ frontend/src/component/pages/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
   }
 
   const handleGoogleLogin = () => {
-    window.location.href = "http://localhost:3000/api/v1/auth/google/login";
+    window.location.href = `${import.meta.env.VITE_API_URL}/auth/google/login`;
   };
 
   const handleLogin = async (e) => {
--- frontend/src/redux/features/auth/authApi.js
+++ frontend/src/redux/features/auth/authApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000/api/v1",
+    baseUrl: import.meta.env.VITE_API_URL,
     credentials: "include", // sends HttpOnly cookie automatically
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.accessToken;
