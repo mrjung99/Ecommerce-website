695af10a4235556f7c7a1aba93eb9bdb1f0a5a6c
zeroCool <jungdaulat99@gmail.com>
2026-05-27 15:37:37 +0545

    added: health route for cron-job

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,7 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './auth/decorator/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Public()
+  @Get('/health')
+  health() {
+    return { status: 'ok' };
+  }
 }
